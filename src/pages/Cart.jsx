d818c3df5cfd41fe60c2f9a8550228ac17f291ad
Kashif <kashifdeshmukh786@gmail.com>
2025-07-28 00:27:08 +0530

    fixed padding

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -100,7 +100,7 @@ function Cart() {
 
       {/* Cart Content */}
       <section className="relative pt-0 sm:pt-12">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
           {/* Header */}
           <div className="text-center mb-12">
             <h1 className="text-4xl lg:text-5xl font-bold mb-6">
