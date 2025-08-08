af99928befa1127f75fbda7aef0f1980f16a18b0
Kashif <kashifdeshmukh786@gmail.com>
2025-08-09 04:18:26 +0530

    Comment out Link component in Navbar to simplify logo display and enhance layout clarity.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
     <nav className="absolute z-50 top-0 left-0 right-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center items-center h-16 sm:h-20">
-          <Link to="/home" className="flex items-center space-x-2 sm:space-x-4 hover:opacity-80 transition-opacity">
+          {/* <Link to="/home" className="flex items-center space-x-2 sm:space-x-4 hover:opacity-80 transition-opacity"> */}
             <img
               src="/brand-logo.png"
               alt="Easy Soul Logo"
               className="w-60 h-18 object-cover"
             />
 
-          </Link>
+          {/* </Link> */}
         </div>
       </div>
     </nav>
