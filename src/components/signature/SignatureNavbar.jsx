1b31385dce0f2ffc83416a676e2fc72f7392f1d9
Kashif <kashifdeshmukh786@gmail.com>
2025-08-09 04:07:57 +0530

    Refactor SignatureNavbar to simplify logo display by removing the Link component, enhancing layout clarity.

--- src/components/signature/SignatureNavbar.jsx
+++ src/components/signature/SignatureNavbar.jsx
@@ -7,14 +7,11 @@ const SignatureNavbar = () => {
     <nav className="signature-navbar">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center items-center h-16 sm:h-20">
-          {/* Centered Brand Logo */}
-          <Link to="/home" className="flex items-center space-x-2 sm:space-x-4 hover:opacity-80 transition-opacity">
-            <img
-              src="/logoT.png"
-              alt="Easy Soul Logo"
-              className="w-32 h-13 object-cover"
-            />
-          </Link>
+          <img
+            src="/logoT.png"
+            alt="Easy Soul Logo"
+            className="w-32 h-13 object-cover"
+          />
         </div>
       </div>
     </nav>
